2da723c5afefda73fce69be0112e68246789321b
liraop <pedrolira@live.com>
2024-07-23 15:19:35 -0300

    Add day6

---
+++ day6/world.py
@@ -0,0 +1,17 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def turn_down():
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    elif wall_on_right():
+        turn_left()
